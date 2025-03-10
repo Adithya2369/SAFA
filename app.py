a701887c78ddf77fd1c0b6e7e1e2fedf21b51a95
Adithya2369 <116473400+Adithya2369@users.noreply.github.com>
2025-03-10 23:57:09 +0530

    test1

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ def process_excel():
 
 @app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('check.html')
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
